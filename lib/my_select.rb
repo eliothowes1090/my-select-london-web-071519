c6ef942bdf689e8ed925a7087c78db23118addb0
Eliot Howes <ejhowes@gmail.com>
2019-05-26 21:47:48 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -5,6 +5,7 @@ def my_select(collection)
   while counter < collection.length
     if yield(collection[counter]) == true
       new_collection.push(collection[counter])
+    end
     counter += 1
   end
   return new_collection
