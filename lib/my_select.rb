e4af4d64f22c7251ba6d17a1ed5025dc8e4b2676
Eliot Howes <ejhowes@gmail.com>
2019-05-26 21:01:10 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,9 @@
 def my_select(collection)
  # your code here!
   counter = 0
+  new_collection = []
   while counter < collection.length
-    yield(collection[counter])
+    yield(new_collection.push(collection[counter]))
     counter += 1
   end
 end
